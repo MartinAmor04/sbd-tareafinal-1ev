4e9de39f7b8ea7e42973f2f53b511a4c8252ef5a
martin <martin.amor2004@gmail.com>
2024-12-11 13:43:56 +0100

    reader in process

---
+++ reader.py
@@ -0,0 +1,62 @@
+# ! pip install pymongo[srv]==4.6.2
+# ! pip install requests
+
+from pymongo import MongoClient
+import time
+import requests
+
+class AtlasClient ():
+
+   def __init__ (self, altas_uri, dbname):
+        self.mongodb_client = MongoClient(altas_uri)
+        self.database = self.mongodb_client[dbname]
+
+   ## Comprobobamos la conexión
+   def ping (self):
+        self.mongodb_client.admin.command('ping')
+
+   def get_collection (self, collection_name):
+        collection = self.database[collection_name]
+        return collection
+
+   def find(self, collection_name, filter={}, projection=None, limit=0):
+        collection = self.get_collection(collection_name)
+        items = list(collection.find(filter=filter, projection=projection, limit=limit))
+        return items
+
+
+ATLAS_URI = 'mongodb+srv://admin:admin@citybik-cluster.2wdih.mongodb.net/?retryWrites=true&w=majority&appName=CityBik-Cluster'
+
+DB_NAME = 'bicisCoruña'
+COLLECTION_NAME = 'info'
+
+atlas_client = AtlasClient (ATLAS_URI, DB_NAME)
+atlas_client.ping()
+print ('Connected to Atlas instance! We are good to go!')
+
+# Conexión al cliente de MongoDB
+client = MongoClient(ATLAS_URI)
+db = client[DB_NAME]
+collection = db[COLLECTION_NAME]
+
+def export_data():
+    stations=atlas_client.find(collection_name=COLLECTION_NAME,projection={"stations": 1,"_id": 0})# Solo traemos la clave "network" porque es la que contiene "stations"
+    for data in stations:
+        id = data.get('id', 'N/A')
+        name = data.get('name','N/A')
+        timestamp = data.get('timestamp', 'N/A')
+        free_bikes = data.get('free_bikes', 'N/A')
+        empty_slots = data.get('empty_slots', 'N/A')
+        print(f'id: {id}\nname: {name}\ntimestamp: {timestamp}\nfree bikes: {free_bikes}\nempty slots: {empty_slots}')
+
+
+def print_sample_document():
+    sample = atlas_client.find(collection_name=COLLECTION_NAME, limit=1)
+    if sample:
+        print("Ejemplo de documento:", sample[0])
+    else:
+        print("No se encontraron documentos.")
+
+# print_sample_document()
+
+export_data()
